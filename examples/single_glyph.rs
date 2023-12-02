0e4a26654ce16a84c9b515e275edad1b7d47e50a
zhengwu <zhengwu@midday.me>
2023-12-02 14:12:32 +0800

    refactor: refactor examples

--- examples/single_glyph.rs
+++ examples/single_glyph.rs
@@ -3,40 +3,37 @@ extern crate unicode_normalization;
 
 use unicode_normalization::UnicodeNormalization;
 
-const WIDTH: usize = 32;
-const HEIGHT: usize = 24;
+const WIDTH: i32 = 32;
+const HEIGHT: i32 = 24;
 
-fn draw_bitmap(bitmap: ft::Bitmap, x: usize, y: usize) -> [[u8; WIDTH]; HEIGHT] {
-    let mut figure = [[0; WIDTH]; HEIGHT];
-    let mut p = 0;
-    let mut q = 0;
+type Figure = [[u8; WIDTH as usize]; HEIGHT as usize];
+
+fn draw_bitmap(bitmap: ft::Bitmap, x: i32, y: i32) -> Figure {
+    let mut figure = [[0; WIDTH as usize]; HEIGHT as usize];
     let w = bitmap.width() as usize;
-    let x_max = x + w;
-    let y_max = y + bitmap.rows() as usize;
+    let x_max = x + w as i32;
+    let y_max = y + bitmap.rows();
 
-    for i in x..x_max {
-        for j in y..y_max {
-            if i < WIDTH && j < HEIGHT {
-                figure[j][i] |= bitmap.buffer()[q * w + p];
-                q += 1;
+    for (p, i) in (x..x_max).enumerate() {
+        for (q, j) in (y..y_max).enumerate() {
+            if i < 0 || j < 0 || i >= WIDTH || j >= HEIGHT {
+                continue;
             }
+            figure[j as usize][i as usize] |= bitmap.buffer()[q * w + p];
         }
-        q = 0;
-        p += 1;
     }
     figure
 }
 
 fn main() {
-    let ref mut args = std::env::args();
-
+    let mut args = std::env::args();
     if args.len() != 3 {
         let exe = args.next().unwrap();
         println!("Usage: {} font character", exe);
         return;
     }
 
-    let ref font = args.nth(1).unwrap();
+    let font = args.nth(1).unwrap();
     let character = args.next().and_then(|s| s.nfc().next()).unwrap() as usize;
     let library = ft::Library::init().unwrap();
     let face = library.new_face(font, 0).unwrap();
@@ -46,21 +43,18 @@ fn main() {
         .unwrap();
 
     let glyph = face.glyph();
-    let x = glyph.bitmap_left() as usize;
-    let y = HEIGHT - glyph.bitmap_top() as usize;
+    let x = glyph.bitmap_left();
+    let y = HEIGHT - glyph.bitmap_top();
     let figure = draw_bitmap(glyph.bitmap(), x, y);
-
-    for i in 0..HEIGHT {
-        for j in 0..WIDTH {
-            print!(
-                "{}",
-                match figure[i][j] {
-                    p if p == 0 => " ",
-                    p if p < 128 => "*",
-                    _ => "+",
-                }
-            );
+    for row in figure {
+        for v in row {
+            let c = match v {
+                0 => " ",
+                1..=127 => "*",
+                _ => "+",
+            };
+            print!("{}", c)
         }
-        println!("");
+        println!(" ");
     }
 }
